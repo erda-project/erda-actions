7f0f10282efbc385e229e09ae748739c3b2ac4ad
sfwn <sfwnlin@gmail.com>
2021-06-17 16:24:34 +0800

    adjust erda-registry.cn-hangzhou.cr.aliyuncs.com to registry.erda.cloud

--- dockerfiles/terminus-herd/herd.1.1.9-beta.2/deploy.sh
+++ dockerfiles/terminus-herd/herd.1.1.9-beta.2/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-image=erda-registry.cn-hangzhou.cr.aliyuncs.com/erda-actions/terminus-herd:1.1.9-beta.2
+image=registry.erda.cloud/erda-actions/terminus-herd:1.1.9-beta.2
 
 docker build . -t ${image}
 docker push ${image}
