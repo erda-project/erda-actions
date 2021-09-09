dc75c1cb680565207c4a61be43843be533762bb7
hustcer <hustcer@outlook.com>
2021-09-09 11:07:44 +0800

    fix: change action meta

--- dockerfiles/terminus-nodejs/node.12.22/deploy.sh
+++ dockerfiles/terminus-nodejs/node.12.22/deploy.sh
@@ -6,4 +6,4 @@ image=registry.erda.cloud/erda-actions/terminus-debian-node:12.22-lts
 docker build . -t ${image} -f Dockerfile.debian.npm.6.14
 docker push ${image}
 
-echo "action meta: terminus-debian-node=$image"
+echo "action meta: terminus-debian-node-12.22=$image"
