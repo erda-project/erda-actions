7aa9162236ac4ed0c949df78728aadbf3656bd7b
hustcer <hustcer@outlook.com>
2021-09-18 06:47:47 +0800

    feature: remove docker images of herd 1.1.8 and 1.1.10 with node 14, add new image herd 1.1.11 with node 14

--- dockerfiles/terminus-herd/herd.1.1.11-debian-node.14.17/deploy.sh
+++ dockerfiles/terminus-herd/herd.1.1.11-debian-node.14.17/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-image=registry.erda.cloud/erda-actions/terminus-debian-herd:1.1.10-n14.17
+image=registry.erda.cloud/erda-actions/terminus-debian-herd:1.1.11-n14.17
 
 docker build . -t ${image}
 docker push ${image}
