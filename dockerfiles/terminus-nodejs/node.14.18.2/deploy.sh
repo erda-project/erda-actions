5371d925f967a4bca7d15dcf320a40645a88b1bf
hustcer <hustcer@outlook.com>
2022-01-07 15:44:16 +0800

    feat: add new dockerfiles for node 14.18-LTS with pnpm installed

---
+++ dockerfiles/terminus-nodejs/node.14.18.2/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+image=registry.erda.cloud/erda-actions/terminus-debian-node:14.18-lts
+
+docker build . -t ${image} -f Dockerfile.debian.npm.6.14
+docker push ${image}
+
+echo "action meta: terminus-debian-node=$image"
