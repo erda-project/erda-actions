b9558da34b53b52b269c6079dfee15bc579e40e3
hustcer <hustcer@outlook.com>
2022-01-07 17:43:48 +0800

    chore: fix typo

--- dockerfiles/terminus-herd/herd.1.1.11-debian-node.14.17/deploy.sh
+++ dockerfiles/terminus-herd/herd.1.1.11-debian-node.14.17/deploy.sh
@@ -6,4 +6,4 @@ image=registry.erda.cloud/erda-actions/terminus-debian-herd:1.1.11-n14.17
 docker build . -t ${image}
 docker push ${image}
 
-echo "action meta: terminus-debian-herd-1.1.11-n14.17==$image"
+echo "action meta: terminus-debian-herd-1.1.11-n14.17=$image"
