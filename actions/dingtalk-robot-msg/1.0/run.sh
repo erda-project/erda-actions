5a25115abb621235cc42b0ad4605d9276133feb3
Effet <nesuadark@gmail.com>
2022-01-17 13:27:15 +0800

    u

--- actions/dingtalk-robot-msg/1.0/run.sh
+++ actions/dingtalk-robot-msg/1.0/run.sh
@@ -6,7 +6,8 @@ set -x
 ts=$(date +%s%3N)
 token=${ACTION_ACCESS_TOKEN}
 sec=${ACTION_SECRET}
-msg=${ACTION_MSG_FILE}
+msg_ctx=${ACTION_MSG_CTX}
+msg_file=${ACTION_MSG_FILE}
 
 if [ -z "${token}" ]; then
   echo "ERROR: access_token is not set"
@@ -18,13 +19,20 @@ if [ -z "${sec}" ]; then
   exit 1
 fi
 
-if [ -z "${msg}" ]; then
-  echo "ERROR: msg is not set"
+if [ -z "${msg_ctx}" ]; then
+  echo "ERROR: msg_ctx is not set"
   exit 1
 fi
+
+if [ -z "${msg_file}" ]; then
+  echo "ERROR: msg_file is not set"
+  exit 1
+fi
+
+jsonnet ${msg_file} --tla-code ctx="${msg_ctx}" -o /tmp/msg.json
 
 echo "INFO: ts: ${ts}"
 
 sig=$(printf "$ts\n$sec" | openssl dgst -sha256 -hmac "$sec" -binary | base64)
 
-curl -XPOST "https://oapi.dingtalk.com/robot/send?access_token=$token&timestamp=$ts&sign=$sig" -H 'Content-Type: application/json' -d@$msg
+curl -XPOST "https://oapi.dingtalk.com/robot/send?access_token=$token&timestamp=$ts&sign=$sig" -H 'Content-Type: application/json' -d@/tmp/msg.json
