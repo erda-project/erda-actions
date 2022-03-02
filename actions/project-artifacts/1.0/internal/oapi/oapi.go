60497cc269a72fce3fd4bb4059004cd93d63b0b0
悟空 <rainchan365@163.com>
2022-03-02 19:03:10 +0800

    feat: remove update project release (#370)
    * feat: delete update
    
    * feat: update image

--- actions/project-artifacts/1.0/internal/oapi/oapi.go
+++ actions/project-artifacts/1.0/internal/oapi/oapi.go
@@ -16,7 +16,6 @@ package oapi
 import (
 	"bytes"
 	"encoding/json"
-	"path/filepath"
 	"strconv"
 
 	"github.com/pkg/errors"
@@ -72,46 +71,6 @@ func GetLatestApplicationRelease(cfg *config.Config, app config.Application) (st
 // CreateUpdateProjectRelease creates the project release if it is not created yet,
 // updates the project release if it is already created.
 func CreateUpdateProjectRelease(cfg *config.Config, releases [][]string) (string, error) {
-	release, ok, err := GetProjectRelease(cfg)
-	if err != nil {
-		return "", errors.Wrapf(err, "failed to GetProjectRelease, projectID(Name): %v(%s), version: %s",
-			cfg.ProjectID, cfg.ProjectName, cfg.Version)
-	}
-	if ok {
-		return release, updateProjectRelease(cfg, release, releases)
-	}
-	return createProjectRelease(cfg, releases)
-}
-
-// GetProjectRelease gets the project release with the given version,
-// if it is not found, return false.
-func GetProjectRelease(cfg *config.Config) (string, bool, error) {
-	var resp ReleasesResponse
-	if _, err := httpclient.New().Get(cfg.OapiHost).
-		Path(oAPI).
-		Header("Authorization", cfg.OapiToken).
-		Param("projectId", strconv.FormatInt(cfg.ProjectID, 10)).
-		Param("isProjectRelease", strconv.FormatBool(true)).
-		Do().
-		JSON(&resp); err != nil {
-		return "", false, errors.Wrap(err, "failed to request")
-	}
-	if !resp.Success {
-		return "", false, errors.Errorf("failed to request: %+v", resp.Error)
-	}
-	if resp.Data == nil || len(resp.Data.List) == 0 {
-		return "", false, nil
-	}
-	for i := range resp.Data.List {
-		if resp.Data.List[i].Version == cfg.Version {
-			return resp.Data.List[i].ReleaseID, true, nil
-		}
-	}
-	return "", false, nil
-}
-
-// createProjectRelease creates the project release and return it's releaseID
-func createProjectRelease(cfg *config.Config, releases [][]string) (string, error) {
 	var request = CreateUpdateReleaseRequest{
 		Version:                cfg.Version,
 		ApplicationReleaseList: releases,
@@ -147,41 +106,4 @@ func createProjectRelease(cfg *config.Config, releases [][]string) (string, erro
 		return "", errors.New("bad response: releaseID is empty")
 	}
 	return resp.Data.ReleaseID, nil
-}
-
-// updateProjectRelease updates the project release
-func updateProjectRelease(cfg *config.Config, releaseID string, releases [][]string) error {
-	var request = CreateUpdateReleaseRequest{
-		Version:                cfg.Version,
-		ApplicationReleaseList: releases,
-		Changelog:              cfg.ChangeLog,
-		OrgId:                  cfg.OrgID,
-		UserId:                 cfg.UserID,
-		ProjectID:              cfg.ProjectID,
-		IsStable:               true,
-		IsFormal:               false,
-		IsProjectRelease:       true,
-	}
-	body, err := json.Marshal(request)
-	if err != nil {
-		return errors.Wrapf(err, "failed to Marshal: %+v", request)
-	}
-	var resp = struct {
-		apistructs.Header
-		Data json.RawMessage `json:"data"`
-	}{}
-
-	response, err := httpclient.New().Put(cfg.OapiHost).
-		Path(filepath.Join(oAPI, releaseID)).
-		RawBody(bytes.NewReader(body)).
-		Header("Authorization", cfg.OapiToken).
-		Do().
-		JSON(&resp)
-	if err != nil {
-		return errors.Wrap(err, "failed to request")
-	}
-	if !resp.Success {
-		return errors.Errorf("failed to request: %+v, data: %s", resp.Error, string(response.Body()))
-	}
-	return nil
 }
