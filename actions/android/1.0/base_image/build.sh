87f77a05ea389e5f275e94e17cf84a301e6034b3
chengjoey <30427474+chengjoey@users.noreply.github.com>
2022-07-22 10:16:29 +0800

    upgrade android node version (#461)

--- actions/android/1.0/base_image/build.sh
+++ actions/android/1.0/base_image/build.sh
@@ -1,2 +1,1 @@
-docker build -t registry.erda.cloud/erda/android-gradle-node:v29 --no-cache \
- --build-arg http_proxy=http://30.43.41.107:1087  --build-arg https_proxy=http://30.43.41.107:1087 .
+docker build -t registry.erda.cloud/erda/android-gradle-node:v30-$(date '+%Y%m%d%H%M%S') --no-cache .
