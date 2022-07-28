252902798bb425d435d52134c5f56d7a2c3c02ca
luobily <luobily@gmail.com>
2022-07-28 10:19:15 +0800

    feature: remove arthas

--- actions/buildpack-aliyun/1.0/internal/run/build/buildartifact/metafile.go
+++ actions/buildpack-aliyun/1.0/internal/run/build/buildartifact/metafile.go
@@ -4,35 +4,37 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/erda-project/erda/apistructs"
+	"github.com/erda-project/erda/pkg/metadata"
+	"github.com/erda-project/erda/pkg/strutil"
+
 	"github.com/erda-project/erda-actions/actions/buildpack-aliyun/1.0/internal/run/bplog"
 	"github.com/erda-project/erda-actions/actions/buildpack-aliyun/1.0/internal/run/pack"
-	"github.com/erda-project/erda/apistructs"
-	"github.com/erda-project/erda/pkg/strutil"
 )
 
 func MetadataFromArtifact(artifact *apistructs.BuildArtifact) string {
 
-	metadata := make(apistructs.Metadata, 0)
+	meta := make(metadata.Metadata, 0)
 
 	// 镜像
 	packResult := make([]pack.ModuleImage, 0)
 	if err := json.Unmarshal([]byte(artifact.Content), &packResult); err == nil {
 		for _, item := range packResult {
-			metadata = append(metadata, apistructs.MetadataField{
+			meta = append(meta, metadata.MetadataField{
 				Name: strutil.Concat(item.ModuleName, "-image"), Value: item.Image,
 			})
 		}
 	}
 
-	metadata = append(metadata,
-		apistructs.MetadataField{Name: "artifact.pipelineID", Value: fmt.Sprintf("%d", artifact.PipelineID)},
-		apistructs.MetadataField{Name: "artifact.type", Value: string(artifact.Type)},
-		apistructs.MetadataField{Name: "artifact.identityText", Value: artifact.IdentityText},
-		apistructs.MetadataField{Name: "artifact.content", Value: artifact.Content},
-		apistructs.MetadataField{Name: "artifact.clusterName", Value: artifact.ClusterName},
+	meta = append(meta,
+		metadata.MetadataField{Name: "artifact.pipelineID", Value: fmt.Sprintf("%d", artifact.PipelineID)},
+		metadata.MetadataField{Name: "artifact.type", Value: string(artifact.Type)},
+		metadata.MetadataField{Name: "artifact.identityText", Value: artifact.IdentityText},
+		metadata.MetadataField{Name: "artifact.content", Value: artifact.Content},
+		metadata.MetadataField{Name: "artifact.clusterName", Value: artifact.ClusterName},
 	)
 
-	resp := Resp{Metadata: metadata}
+	resp := Resp{Metadata: meta}
 	respb, err := json.MarshalIndent(resp, "", "  ")
 	if err != nil {
 		bplog.Printf("failed to unmarshal metadata, err: %v\n", err)
@@ -42,5 +44,5 @@ func MetadataFromArtifact(artifact *apistructs.BuildArtifact) string {
 }
 
 type Resp struct {
-	Metadata apistructs.Metadata `json:"metadata"`
+	Metadata metadata.Metadata `json:"metadata"`
 }
