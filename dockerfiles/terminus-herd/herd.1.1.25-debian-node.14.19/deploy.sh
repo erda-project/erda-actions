23d867c13910378d1d0a16e5d234c8f9c70e3596
sfwn <sfwnlin@gmail.com>
2023-06-29 11:30:49 +0800

    feat: add terminus-herd 1.1.21 & 1.1.25 (#5)

---
+++ dockerfiles/terminus-herd/herd.1.1.25-debian-node.14.19/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+image=registry.erda.cloud/erda-actions/terminus-debian-herd:1.1.25-n14.19
+
+docker buildx build --platform linux/amd64 -t ${image} --push . -f Dockerfile
+
+echo "action meta: terminus-debian-herd-1.1.25-n14.19=$image"
