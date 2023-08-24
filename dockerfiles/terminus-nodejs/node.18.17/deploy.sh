639e2d4fc1b11024c8d7a1a4af9e9ad6aa6f69bc
Justin Ma <hustcer@outlook.com>
2023-08-24 14:20:04 +0800

    Add node v18 support (#18)
    * feat: Add node v18 support for js-build and js-pack action
    
    * feat: Add node.18.17/Dockerfile.debian.npm.9.6
    
    * Add herd.1.1.26-debian-node.18.17/Dockerfile
    
    * Update herd.1.1.26-debian-node.18.17/Dockerfile

---
+++ dockerfiles/terminus-nodejs/node.18.17/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+image=registry.erda.cloud/erda-actions/terminus-debian-node:18.17-lts
+
+docker buildx build --platform linux/amd64 -t ${image} --push . -f Dockerfile.debian.npm.9.6
+
+echo "action meta: terminus-debian-node=$image"
