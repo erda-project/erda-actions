09345c4fbeba6c6185d30be6fee14232a628549f
sfwn <sfwnlin@gmail.com>
2023-08-30 11:55:32 +0800

    feat: add js-build 2.0 (#23)
    * base node.18.17 image support multi-arch
    
    * add js-build 2.0, soft-links many files from 1.0

--- dockerfiles/terminus-nodejs/node.18.17/deploy.sh
+++ dockerfiles/terminus-nodejs/node.18.17/deploy.sh
@@ -3,6 +3,6 @@ set -eo pipefail
 
 image=registry.erda.cloud/erda-actions/terminus-debian-node:18.17-lts
 
-docker buildx build --platform linux/amd64 -t ${image} --push . -f Dockerfile.debian.npm.9.6
+docker buildx build --platform linux/amd64,linux/arm64 -t ${image} --push . -f Dockerfile.debian.npm.9.6
 
 echo "action meta: terminus-debian-node=$image"
