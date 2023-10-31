b838387355e7634a62ffb9b26e0e1e69408aa728
hustcer <hustcer@outlook.com>
2023-10-31 10:11:35 +0800

    feat: Add more build platform support for node and herd images

--- dockerfiles/terminus-herd/herd.1.1.27-debian-node.14.21/deploy.sh
+++ dockerfiles/terminus-herd/herd.1.1.27-debian-node.14.21/deploy.sh
@@ -3,6 +3,6 @@ set -eo pipefail
 
 image=registry.erda.cloud/erda-actions/terminus-debian-herd:1.1.27-n14.21
 
-docker buildx build --platform linux/amd64 -t ${image} --push . -f Dockerfile
+docker buildx build --platform linux/amd64,linux/arm64 -t ${image} --push . -f Dockerfile
 
 echo "action meta: terminus-debian-herd-1.1.27-n14.21=$image"
--- dockerfiles/terminus-nodejs/node.14.19.3/deploy.sh
+++ dockerfiles/terminus-nodejs/node.14.19.3/deploy.sh
@@ -3,6 +3,6 @@ set -eo pipefail
 
 image=registry.erda.cloud/erda-actions/terminus-debian-node:14.19-lts
 
-docker buildx build --platform linux/amd64 -t ${image} --push . -f Dockerfile.debian.npm.6.14
+docker buildx build --platform linux/amd64,linux/arm64 -t ${image} --push . -f Dockerfile.debian.npm.6.14
 
 echo "action meta: terminus-debian-node=$image"
