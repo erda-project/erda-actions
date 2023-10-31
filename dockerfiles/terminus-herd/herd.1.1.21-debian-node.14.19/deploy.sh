3aadac35d0e3a59286da0aed78be406e0223755c
Justin Ma <hustcer@outlook.com>
2023-10-31 10:14:38 +0800

    feat: Add herd.1.1.27-debian-node.14.21/Dockerfile (#29)
    * feat: Add herd.1.1.27-debian-node.14.21/Dockerfile
    
    * chore: Update docker file
    
    * feat: Add more build platform support for node and herd images

--- dockerfiles/terminus-herd/herd.1.1.21-debian-node.14.19/deploy.sh
+++ dockerfiles/terminus-herd/herd.1.1.21-debian-node.14.19/deploy.sh
@@ -3,6 +3,6 @@ set -eo pipefail
 
 image=registry.erda.cloud/erda-actions/terminus-debian-herd:1.1.21-n14.19
 
-docker buildx build --platform linux/amd64 -t ${image} --push . -f Dockerfile
+docker buildx build --platform linux/amd64,linux/arm64 -t ${image} --push . -f Dockerfile
 
 echo "action meta: terminus-debian-herd-1.1.21-n14.19=$image"
---
+++ dockerfiles/terminus-herd/herd.1.1.27-debian-node.14.21/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+image=registry.erda.cloud/erda-actions/terminus-debian-herd:1.1.27-n14.21
+
+docker buildx build --platform linux/amd64,linux/arm64 -t ${image} --push . -f Dockerfile
+
+echo "action meta: terminus-debian-herd-1.1.27-n14.21=$image"
