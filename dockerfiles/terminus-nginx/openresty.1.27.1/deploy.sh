e569e7643866a3f53fffdaf58e2da14c4b079c73
Justin Ma <hustcer@outlook.com>
2025-07-21 09:40:31 +0800

    chore: Bump to nginx:1.27.1.2 (#69)

--- dockerfiles/terminus-nginx/openresty.1.27.1/deploy.sh
+++ dockerfiles/terminus-nginx/openresty.1.27.1/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-image=registry.erda.cloud/erda-actions/nginx:1.27.1
+image=registry.erda.cloud/erda/nginx:1.27.1.2
 
 docker buildx build --platform linux/amd64,linux/arm64 -t ${image} --push . -f Dockerfile
 
-echo "action meta: nginx-1.27.1=$image"
+echo "action meta: nginx-1.27.1.2=$image"
