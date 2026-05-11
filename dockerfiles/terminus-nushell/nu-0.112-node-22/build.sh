b6c6528e2de05d7f027e35813f0b52f1e9af9ee5
hustcer <hustcer@outlook.com>
2026-05-11 16:33:46 +0800

    chore: Update docker image build script

--- dockerfiles/terminus-nushell/nu-0.112-node-22/build.sh
+++ dockerfiles/terminus-nushell/nu-0.112-node-22/build.sh
@@ -1,5 +1,6 @@
-
+#!/usr/bin/env bash
+set -eo pipefail
 image=registry.erda.cloud/erda/terminus-debian-nu:0.112-node.22.22.lts
-
-docker build -t ${image} .
-docker push ${image}
+# 使用 buildx 构建并同时推送 amd64 和 arm64 双架构镜像
+docker buildx build --platform linux/amd64,linux/arm64 -t ${image} --push .
+echo "action meta: terminus-debian-nu=$image"
